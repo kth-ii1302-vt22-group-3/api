6e39268d5e900c48629a72b5c6d10345df0531a3
arifjehoh <arifjeh.oh@gmail.com>
2022-04-08 12:26:05 +0200

    #5: Deconstructing the routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
-export const routes = (app) => {
+const defaultRoutes = (app) => {
     app.get("/", (req, res) => res.send("Hello World!"));
     app.get("/hello", (req, res) => res.send("World"));
     app.get("/ping", (req, res) => res.send("pong"));
     app.get("/message", (req, res) => res.json({ message: "Hello World!" }));
+};
+
+    
+
+export const routes = (app) => {
+    defaultRoutes(app);
 };
