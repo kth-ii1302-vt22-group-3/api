464ffc2a25bb35e16bd54b0304d918d35392abf6
arifjehoh <arifjeh.oh@gmail.com>
2022-04-08 12:22:06 +0200

    #5: /message return a json

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,5 @@ export const routes = (app) => {
     app.get("/", (req, res) => res.send("Hello World!"));
     app.get("/hello", (req, res) => res.send("World"));
     app.get("/ping", (req, res) => res.send("pong"));
+    app.get("/message", (req, res) => res.json({ message: "Hello World!" }));
 };
