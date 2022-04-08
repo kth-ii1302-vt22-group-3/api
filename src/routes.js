aa4414351698810a94a44a93efc6d6dc62fd4178
arifjehoh <arifjeh.oh@gmail.com>
2022-04-08 12:39:43 +0200

    #5: TemperatureRoutes added with object variable

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 const defaultRoutes = (app) => {
-    app.get("/", (req, res) => res.send("Hello World!"));
-    app.get("/hello", (req, res) => res.send("World"));
-    app.get("/ping", (req, res) => res.send("pong"));
-    app.get("/message", (req, res) => res.json({ message: "Hello World!" }));
+  app.get("/", (req, res) => res.send("Hello World!"));
+  app.get("/hello", (req, res) => res.send("World"));
+  app.get("/ping", (req, res) => res.send("pong"));
+  app.get("/message", (req, res) => res.json({ message: "Hello World!" }));
 };
 
-    
+const temperatureRoutes = (app) => {
+  app.get("/temperature", (req, res) =>
+    res.json({
+        id: 1,
+        timestamp: new Date().toISOString(),
+        value: 20.1,
+        unit: "Celsius",
+    })
+  );
+};
 
 export const routes = (app) => {
-    defaultRoutes(app);
+  defaultRoutes(app);
+  temperatureRoutes(app);
 };
