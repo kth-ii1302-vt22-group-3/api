4fac98e0e5cc9e042f0a1e758f45ae3ad447f2d3
arifjehoh <arifjeh.oh@gmail.com>
2022-04-08 14:42:43 +0200

    #8: Express app uses json format

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { routes } from "./routes.js";
 import express from "express";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT || 3000;
 
 routes(app);
