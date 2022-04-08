6acd92120b595860b059429fb7f2ad135f86abfc
arifjehoh <arifjeh.oh@gmail.com>
2022-04-08 14:24:23 +0200

    #7: Remove comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-// Create a new instance of the express and start it up on env port or default port 3000 if none is provided in the env file 
 import { routes } from "./routes.js";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Setup route for ping and hello
 routes(app);
 
-// Start the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
