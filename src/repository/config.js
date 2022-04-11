bc5cf74a901db169ca01e166706fca058e2dff4a
arifjehoh <arifjeh.oh@gmail.com>
2022-04-11 15:28:39 +0200

    #11: ENV variable added for DATABASE URL

--- src/repository/config.js
+++ src/repository/config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const URI = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/postgres';
+const URI = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/ii1302';
 const sequelize = new Sequelize(URI);
 
 module.exports = sequelize;
