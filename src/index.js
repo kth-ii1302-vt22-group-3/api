693521d2a3b7789d8df5f62d82529e3e5cbd6bb2
arifjehoh <arifjeh.oh@gmail.com>
2022-04-28 14:58:54 +0200

    #17: Enable cors to all

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const routes = require("./routes.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 routes(app);
 
